6ea746152213e04e2c1e1d3a006189b79ac202ad
raffi306 <84123921+raffi306@users.noreply.github.com>
2021-05-13 01:55:28 -0700

    Add files via upload

---
+++ handlers/pmpermit.py
@@ -0,0 +1,28 @@
+# Daisyxmusic (Telegram bot project )
+# Copyright (C) 2021  Inukaasith 
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+
+
+
+from callsmusic.callsmusic import client as USER
+from pyrogram import filters
+from pyrogram.types import Chat, Message, User
+
+
+@USER.on_message(filters.text & filters.private & ~filters.me & ~filters.bot)
+async def pmPermit(client: USER, message: Message):
+  await USER.send_message(message.chat.id,"اهلاً ياصاح قم بـ اضافتي الى مجموعتك الخاصه مع البوت المساعد @SYwBoT وان واجهتك مشاكل اخبر المطور @MayBeRaFFi")
+  return                        
